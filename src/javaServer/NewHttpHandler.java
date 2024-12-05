816e2abe33ec1be12f24ba318051b789b7372092
jcmalmeida <joaocarlosma@gmail.com>
2024-12-05 08:17:47 -0300

    fix: correções para funcionamento correto de POST, PUT e DELETE

--- src/javaServer/NewHttpHandler.java
+++ src/javaServer/NewHttpHandler.java
@@ -7,7 +7,6 @@ public class NewHttpHandler {
 
     public static String handleRequest(User user, String request) {
         System.out.println("Current user: " + user.getName());
-        System.out.println("Requisição: " + request);
         String req = "";
         try {
             String requestLine = request.split("\n")[0];
@@ -72,11 +71,13 @@ public class NewHttpHandler {
         MyName myName = gson.fromJson(json, MyName.class);
         user.setName(myName.name);
 
+        String body = "POST recebido com sucesso";
         String response = "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/plain\r\n" +
-                "Content-Length: 25\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Connection: keep-alive\r\n" +
                 "\r\n" +
-                "POST recebido com sucesso";
+                body;
         user.receive(response);
     }
 
@@ -88,22 +89,27 @@ public class NewHttpHandler {
         MyName myName = gson.fromJson(json, MyName.class);
         user.setName(myName.name);
 
+        String body = "PUT recebido com sucesso";
         String response = "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/plain\r\n" +
-                "Content-Length: 25\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Connection: keep-alive\r\n" +
                 "\r\n" +
-                "PUT recebido com sucesso";
+                body;
         user.receive(response);
     }
 
     private static void handleDelete(User user, String request) throws Exception {
         System.out.println("Dados do DELETE recebidos: " + request);
 
+        String body = "Entidade deletada do servidor";
         String response = "HTTP/1.1 200 OK\r\n" +
                 "Content-Type: text/plain\r\n" +
-                "Content-Length: 18\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Connection: close\r\n" +
                 "\r\n" +
-                "Entidade deletada do servidor, sessão encerrada";
+                body;
+
         user.receive(response);
     }
 
